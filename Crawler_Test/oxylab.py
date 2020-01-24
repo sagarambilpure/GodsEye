24e7635c65e7380c7753b91111ba175a465ee29f
Uzair Chhapra <41239986+uzairchhapra@users.noreply.github.com>
2020-01-24 22:31:03 +0530

    urls without social done

--- Crawler_Test/oxylab.py
+++ Crawler_Test/oxylab.py
@@ -4,74 +4,57 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# writer = csv.writer(open('output.csv', 'w+', encoding='utf-8-sig', newline=''))
-# writer.writerow(['Name', 'Position', 'Company', 'Education', 'Location', 'URL'])
-
-
 driver = webdriver.Chrome('chromedriver')
 driver.maximize_window()
 driver.get('https://oxylabs.io/products/real-time-crawler')
 
 keyword = driver.find_element_by_name("keyword")
-keyword.send_keys('Uzair Chhapra')
+keyword.send_keys('Aditya Chavan')
 sleep(0.5)
 
-# password = driver.find_element_by_name('session_password')
-# password.send_keys('Abc@12345')
-# sleep(0.5)
-
-# sign_in_button = driver.find_element_by_class_name('button is-primary is-vcentered')
-# sign_in_button.click()
 keyword.submit()
-sleep(10)
-text = driver.find_element_by_tag_name("pre")
-print("/n/n/n/n/n/n",text.text,"/n/n/n/n/n/n/n")
-# driver.get('https://www.google.com/')
-# search_query = driver.find_element_by_name('q')
-# search_query.send_keys('site:linkedin.com/in AND "python developer" AND "london"')
-# search_query.send_keys(Keys.RETURN)
-# sleep(0.5)
+contentString=""
+while len(contentString)<25:
+    contentString = driver.find_element_by_tag_name("pre").text
 
-# urls = driver.find_elements_by_xpath('//*[@class = "r"]/a[@href]')
-# urls = [url.get_attribute('href') for url in urls]
-# sleep(0.5)
-
-# for url in urls:
-#     driver.get(url)
-#     sleep(2)
-
-#     sel = Selector(text = driver.page_source)
-
-#     name = sel.xpath('//*[@class = "inline t-24 t-black t-normal break-words"]/text()').extract_first().split()
-#     name = ' '.join(name)
-
-#     position = sel.xpath('//*[@class = "mt1 t-18 t-black t-normal"]/text()').extract_first().split()
-#     position = ' '.join(position)
-
-#     experience = sel.xpath('//*[@class = "pv-top-card-v3--experience-list"]')
-#     company = experience.xpath('./li[@data-control-name = "position_see_more"]//span/text()').extract_first()
-#     company = ''.join(company.split()) if company else None
-#     education = experience.xpath('.//li[@data-control-name = "education_see_more"]//span/text()').extract_first()
-#     education = ' '.join(education.split()) if education else None
-
-#     location = ' '.join(sel.xpath('//*[@class = "t-16 t-black t-normal inline-block"]/text()').extract_first().split())
-
-#     url = driver.current_url
-
-#     print('\n')
-#     print('Name: ', name)
-#     print('Position: ', position)
-#     print('Company: ', company)
-#     print('Education: ', education)
-#     print('Location: ', location)
-#     print('URL: ', url)
-#     print('\n')
-          
-#     writer.writerow([name,
-#                  position,
-#                  company,
-#                  education,
-#                  location,
-#                  url])
-          
 driver.quit()
+
+#Analysis of contentString
+url=''
+source=''
+for i in range(len(contentString)):
+    if contentString[i]=='u' and contentString[i+1]=='r' and contentString[i+2]=='l' and contentString[i+3]=='"' and contentString[i+4]==':':
+        for j in range(i+7, len(contentString)):
+            url+=contentString[j]
+            if contentString[j]=='"':
+                break
+    if contentString[i]=='s' and contentString[i+1]=='o' and contentString[i+2]=='u' and contentString[i+3]=='r' and contentString[i+4]=='c' and contentString[i+5]=='e' and contentString[i+6]=='"':
+        for j in range(i+10, len(contentString)):
+            source+=contentString[j]
+            if contentString[j]=='"':
+                break
+
+url=url.split("\"")
+
+source=source.split('"')
+
+url=url+source
+z=''
+url=set(url)
+url=list(url)
+url.remove(z)
+# print('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+# print(url)
+# print('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+
+#Removing Social Sites
+urlNoSocial=url
+social=['facebook', 'twitter']
+for e in urlNoSocial:
+    for s in social:
+        if s in e:
+            urlNoSocial.remove(e)
+        
+for i in urlNoSocial:
+    print(i)
+
