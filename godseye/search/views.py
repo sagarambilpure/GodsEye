ba4cd5f132ceadcfcac05e2e1441e9b53bff4f60
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2020-01-24 22:02:24 +0530

    basic

--- godseye/search/views.py
+++ godseye/search/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("wow")
+    else:
+        return render(request, 'search/index.html')
+
+
+
